3d9c752c552d08f606e26ce2408b41581121d6bf
Bruh <Bruh@DESKTOP-LCEIPVS>
2021-09-21 15:51:31 +0300

    brand name changes

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -40,7 +40,7 @@ import cartIcon from '../../assets/shopping_cart.svg'
                         history.push(SHOP_ROUTE)
                     }}
                 >
-                    Aboba Shop
+                    A Shop
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
